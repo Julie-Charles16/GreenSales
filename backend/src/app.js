2867b26225de701dcad8b4032838ae54b4a3f4ff
Julie-Charles16 <juliecharles2716@gmail.com>
2026-04-04 18:17:08 +0200

    Initialisation backend : express, dotenv, config DB

---
+++ backend/src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+const clientRoutes = require('./routes/clientRoute');
+const appointmentRoutes = require('./routes/appointmentRoute');
+const saleRoutes = require('./routes/saleRoute');
+
+app.use('/clients', clientRoutes);
+app.use('/appointments', appointmentRoutes);
+app.use('/sales', saleRoutes);
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('API GreenSales OK');
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Serveur lancé sur le port ${PORT}`);
+});
